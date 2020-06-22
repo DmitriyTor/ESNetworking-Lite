0c28aa9668a24b58070f484f884bd8210af2f08f
Dmitriy <ToropkinD@gmail.com>
2020-06-22 11:59:13 +0300

    [in-progress] ooouups

--- Sources/ESNetworking-Lite/Models/RequestModel.swift
+++ Sources/ESNetworking-Lite/Models/RequestModel.swift
@@ -9,9 +9,17 @@ import Foundation
 
 open class _RequestModel {
     
-    open var urlParameters = [String: Any]()
+    open var urlParameters: [String: Any] {
+        return [String: Any]()
+    }
     
-    open var bodyParameters = [String: Any]()
+    open var bodyParameters: [String: Any] {
+        return [String: Any]()
+    }
     
-    open var headers = [ESHeader.content_type.rawValue: "application/json"]
+    open var headers: [String: String] {
+        return [
+            ESHeader.content_type.rawValue: "application/json"
+        ]
+    }
 }
