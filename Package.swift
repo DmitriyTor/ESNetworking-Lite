50ea994187c60d5a73a5bb7809b4190317b477b2
Dmitriy <ToropkinD@gmail.com>
2020-06-22 12:40:41 +0300

    [in-progress] add jazzy doc

--- Package.swift
+++ Package.swift
@@ -6,23 +6,20 @@ import PackageDescription
 let package = Package(
     name: "ESNetworking-Lite",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ESNetworking-Lite",
             targets: ["ESNetworking-Lite"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ESNetworking-Lite",
-            dependencies: []),
+            dependencies: [],
+            exclude: ["docs"]),
         .testTarget(
             name: "ESNetworking-LiteTests",
-            dependencies: ["ESNetworking-Lite"]),
+            dependencies: ["ESNetworking-Lite"],
+            exclude: ["docs"]),
     ]
 )
